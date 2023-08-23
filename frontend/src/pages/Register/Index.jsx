edba6486dd899f61ff4febdc840f8af951a427fd
kaushik.bora <herock76@gmail.com>
2023-08-23 22:17:59 +0530

    -initial commit

--- frontend/src/pages/Register/Index.jsx
+++ frontend/src/pages/Register/Index.jsx
@@ -1,74 +1,47 @@
-import { Button, Checkbox, Form, Input } from 'antd';
+import Button from '../../components/Button';
+import { Link } from 'react-router-dom';
+import { Form } from 'antd';
 
 const Register = () => {
-  const onFinish = (values) => {
-    console.log('Success:', values);
-  };
-  const onFinishFailed = (errorInfo) => {
-    console.log('Failed:', errorInfo);
-  };
   return (
-    <Form
-      name='basic'
-      labelCol={{
-        span: 8,
-      }}
-      wrapperCol={{
-        span: 16,
-      }}
-      style={{
-        maxWidth: 600,
-      }}
-      initialValues={{
-        remember: true,
-      }}
-      onFinish={onFinish}
-      onFinishFailed={onFinishFailed}
-      autoComplete='off'>
-      <Form.Item
-        label='Username'
-        name='username'
-        rules={[
-          {
-            required: true,
-            message: 'Please input your username!',
-          },
-        ]}>
-        <Input />
-      </Form.Item>
+    <div className='flex justify-center h-screen items-center bg-primary'>
+      <div className='card p-3 w-400'>
+        <h1 className='text-xl mb-1'>
+          Welcome to Scaler Shows! Please Register{' '}
+        </h1>
+        <hr />
+        <Form layout='vertical' className='mt-1'>
+          <Form.Item
+            label='Name'
+            name='name'
+            rules={[{ required: true, message: 'Please input your name!' }]}>
+            <input type='text' />
+          </Form.Item>
+          <Form.Item
+            label='Email'
+            name='email'
+            rules={[{ required: true, message: 'Please input your email!' }]}>
+            <input type='email' />
+          </Form.Item>
+          <Form.Item
+            label='Password'
+            name='password'
+            rules={[
+              { required: true, message: 'Please input your password!' },
+            ]}>
+            <input type='password' />
+          </Form.Item>
 
-      <Form.Item
-        label='Password'
-        name='password'
-        rules={[
-          {
-            required: true,
-            message: 'Please input your password!',
-          },
-        ]}>
-        <Input.Password />
-      </Form.Item>
-
-      <Form.Item
-        name='remember'
-        valuePropName='checked'
-        wrapperCol={{
-          offset: 8,
-          span: 16,
-        }}>
-        <Checkbox>Remember me</Checkbox>
-      </Form.Item>
-
-      <Form.Item
-        wrapperCol={{
-          offset: 8,
-          span: 16,
-        }}>
-        <Button type='primary' htmlType='submit'>
-          Submit
-        </Button>
-      </Form.Item>
-    </Form>
+          <div className='flex flex-col mt-2 gap-1'>
+            <Button fullWidth title='REGISTER' type='submit' />
+            <Link to='/login' className='text-primary'>
+              {' '}
+              Already have an account? Login
+            </Link>
+          </div>
+        </Form>
+      </div>
+    </div>
   );
 };
 
