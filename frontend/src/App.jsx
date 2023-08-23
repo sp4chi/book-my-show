055baff9ee0e3984bc9f0093bb6d878dffdc3d93
kaushik.bora <herock76@gmail.com>
2023-08-24 01:42:53 +0530

    -added styles, dotenv nodemon, roboto

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,13 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import Home from './pages/Home/Index';
 import Login from './pages/Login/Index';
 import Register from './pages/Register/Index';
+
+import './stylesheets/alignments.css';
+import './stylesheets/sizes.css';
+import './stylesheets/form-elements.css';
+import './stylesheets/theme.css';
+import './stylesheets/custom.css';
+
 function App() {
   const router = createBrowserRouter([
     { path: '/', element: <Home /> },
