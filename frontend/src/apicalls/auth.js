36d450ead6d3ad538834b0720749d6bc2c8d7827
kaushik.bora <herock76@gmail.com>
2024-05-30 17:35:17 +0530

    -minor changes

--- frontend/src/apicalls/auth.js
+++ frontend/src/apicalls/auth.js
@@ -2,18 +2,22 @@ import { axiosInstance } from './axiosInstance.js';
 
 export const registerUser = async (payload) => {
   try {
-    const response = await axiosInstance.post('auth/register', payload);
+    const response = await axiosInstance.post('/auth/register', payload);
+    console.log(response.data);
     return response.data;
   } catch (error) {
+    console.log(error.response.data);
     return error.response.data;
   }
 };
 
 export const loginUser = async (payload) => {
   try {
-    const response = await axiosInstance.post('auth/login', payload);
+    const response = await axiosInstance.post('/auth/login', payload);
+    console.log(response.data);
     return response.data;
   } catch (error) {
+    console.log(error.response.data);
     return error.response.data;
   }
 };
