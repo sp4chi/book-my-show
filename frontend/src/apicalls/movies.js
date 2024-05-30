6d7b3cd844ba6b57264e014fc0ef4a06935510eb
kaushik.bora <herock76@gmail.com>
2024-05-31 03:58:30 +0530

    -movie edit and delete api calls

--- frontend/src/apicalls/movies.js
+++ frontend/src/apicalls/movies.js
@@ -21,3 +21,29 @@ export const GetAllMovies = async () => {
     return error.response.data;
   }
 };
+
+export const DeleteMovie = async (payload) => {
+  try {
+    const response = await axiosInstance.delete(`/movies/delete/${payload}`);
+
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.log(error.response.data);
+    return error.response.data;
+  }
+};
+
+export const UpdateMovie = async (payload) => {
+  try {
+    const response = await axiosInstance.patch(
+      `/movies/edit/${payload.movieId}`,
+      payload
+    );
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.log(error.response.data);
+    return error.response.data;
+  }
+};
