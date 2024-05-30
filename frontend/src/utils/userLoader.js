38a551b3163bc25637d12d0f78438a061421da34
kaushik.bora <herock76@gmail.com>
2024-05-30 17:35:48 +0530

    -removed bugs and refactored

--- frontend/src/utils/userLoader.js
+++ frontend/src/utils/userLoader.js
@@ -2,20 +2,41 @@ import { redirect } from 'react-router-dom';
 import { GetCurrentUser } from '../apicalls/user';
 import { SetUser } from '../redux/userSlice';
 import { HideLoading, ShowLoading } from '../redux/loaderSlice';
+import { message } from 'antd';
 
 export async function loader(dispatch) {
+  if (localStorage.getItem('token')) {
+    return getpresentUser(dispatch);
+  }
+  return redirect('/login');
+}
+
+const getpresentUser = async (dispatch) => {
   try {
     dispatch(ShowLoading());
+
     const response = await GetCurrentUser();
+
     if (response.status === 'success') {
       dispatch(SetUser(response.data));
-      return response.data;
-    }
 
-    return redirect('/login');
+      return response.data;
+    } else {
+      dispatch(SetUser(null));
+
+      message.error(response.message);
+
+      localStorage.removeItem('token');
+
+      return redirect('/login');
+    }
   } catch (error) {
     dispatch(SetUser(null));
+
+    message.error(error.message);
+
+    return null;
   } finally {
     dispatch(HideLoading());
   }
-}
+};
